e55ae6a624aa4558bf03ad4bcc2562c4f0deca1f
Asif-102 <aiqbal517@gmail.com>
2021-09-18 03:19:06 +0600

    added core functionality

--- js/app.js
+++ js/app.js
@@ -1,20 +1,39 @@
 const calculate = [];
+const calcInput = document.getElementById('typed-numbers');
 
 document.getElementById('key-pad').addEventListener('click', function (event) {
     const number = event.target.innerText;
-    const calcInput = document.getElementById('typed-numbers');
+
 
     if (isNaN(number)) {
-        if(number == 'C'){
+        if (number == 'C') {
             calcInput.value = '';
+            calculate.length = null;
         }
-        else if(number == '<'){
+        else if (number == '<') {
             calcInput.value = calcInput.value.slice(0, -1);
+            calculate.pop();
         }
+        else if (number == '+' || number == '-' || number == '*' || number == '/') {
+            if (calculate[calculate.length - 1] != '+' && calculate[calculate.length - 1] != '-' && calculate[calculate.length - 1] != '*' && calculate[calculate.length - 1] != '/') {
+                updateInputAndArray(number)
+            }
+        }
+    }
+    else {
+        updateInputAndArray(number)
+    }
+})
+
+function updateInputAndArray(number) {
+    calculate.push(number)
+    if (calculate[0] == '+' || calculate[0] == '-' || calculate[0] == '*' || calculate[0] == '/') {
+        calculate.pop();
     }
     else {
         const previousNumber = calcInput.value;
         const newNumber = previousNumber + number;
         calcInput.value = newNumber;
     }
-})
+    
+}
